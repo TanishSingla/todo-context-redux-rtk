46f2ac2f824dd7cd4f5ce17246c270d67301dc4d
Tanish <tanish.singla@anywhere.co>
2025-12-15 16:38:22 +0530

    Todo app using RTK

---
+++ toolkit/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import TodoItem from "./features/Todo/TodoItem";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, editTodo } from "./features/Todo/todoSlice";
+
+const App = () => {
+  const [inp, setInp] = useState("");
+  const [editId, setEditId] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleAddOrEditTodo = () => {
+    if (!inp.trim()) return;
+
+    if (editId === null) {
+      dispatch(addTodo(inp));
+      setInp("");
+    } else {
+      dispatch(editTodo({ id: editId, title: inp }));
+      setInp("");
+    }
+  };
+
+  const handleEditClick = (id, title) => {
+    setInp(title);
+    setEditId(id);
+  };
+
+  const todos = useSelector((state) => state.todo.todos);
+  console.log(todos);
+  return (
+    <div>
+      <div>
+        <h3>Add Todo</h3>
+        <input
+          type="text"
+          value={inp}
+          onChange={(e) => setInp(e.target.value)}
+        />
+        <button onClick={handleAddOrEditTodo}>{editId ? "Edit" : "Add"}</button>
+      </div>
+      <div>
+        <h2>Todo List - Redux Toolkit</h2>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleEditClick={handleEditClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
